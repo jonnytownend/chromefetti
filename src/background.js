247ead4f89b22ee04dfb1247581104575603427c
Jonny Townend <jonny@brightec.co.uk>
2022-02-22 12:06:51 +0000

    Check if confetti can be shown for tab utl

--- src/background.js
+++ src/background.js
@@ -1,6 +1,10 @@
 let lastTabId = -1
 let confettiShown = false
 
+function pageIsAllowed(url) {
+  return !url.startsWith('chrome://') && !url.startsWith('chrome-extension://')
+}
+
 // On first install, set confettiEveryPage to true
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ confettiEveryPage: true }, () => {})
@@ -15,7 +19,7 @@ chrome.tabs.onCreated.addListener((tab) => {
 // On tab update, show confetti if required
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   chrome.storage.sync.get('confettiEveryPage', ({ confettiEveryPage }) => {
-    if (confettiEveryPage && tab.url.split(':')[0] !== 'chrome' && tabId === lastTabId && !confettiShown) {
+    if (confettiEveryPage && pageIsAllowed(tab.url) && tabId === lastTabId && !confettiShown) {
       chrome.scripting.executeScript({
         target: {
           tabId: tab.id,
@@ -34,7 +38,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const tab = tabs[0]
-    if (tab.url.split(':')[0] !== 'chrome') {
+    if (pageIsAllowed(tab.url)) {
       chrome.scripting.executeScript({
         target: {
           tabId: tab.id,
